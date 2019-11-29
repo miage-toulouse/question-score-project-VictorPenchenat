52cdea818291e92dcfa39bce2c7c1bc1651354d8
VictorPenchenat <victor.penchenat@gmail.com>
2019-11-29 17:24:05 +0100

    fix #2 Tests sur QuestionAChoixMultiple

---
+++ src/test/java/miagem1/ScoreCalculateurTest.java
@@ -0,0 +1,30 @@
+package miagem1;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class ScoreCalculateurTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(ScoreCalculateur.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void calculeScore() {
+    }
+}
